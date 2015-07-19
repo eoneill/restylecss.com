03a397a3d9172bbbcf9a930cff42c5a3790be92d
Eugene ONeill <oneill.eugene@gmail.com>
2015-07-19 09:27:50 -0700

    cleanup

---
+++ metalsmith/index.js
@@ -0,0 +1,79 @@
+var Metalsmith = require("metalsmith");
+var Handlebars = require("handlebars");
+
+function debug(files, metalsmith, done) {
+  console.log("[metalsmith]", files);
+  done();
+}
+
+module.exports = function(rootDir) {
+  var config = require("./config");
+  var plugins = require("./plugins")(config);
+  var helpers = require("./helpers")(Handlebars, config);
+  var partials = require("./partials")(Handlebars);
+
+  new Metalsmith(rootDir)
+    .metadata(config)
+    .use(plugins.filter())
+    .use(plugins.drafts())
+    .use(plugins.watch({
+      paths: {
+        // TODO - this isn't really working right, need to adjust the patterns
+        "${source}/**/*": true,
+        "${source}/styles/**/*": "**/*",
+        "layouts/**/*": "**/*",
+      },
+      livereload: true
+    }))
+    .use(plugins.collections({
+      posts: {
+        pattern: "content/posts/*.md",
+        sortBy: "date",
+        reverse: true
+      }
+    }))
+    .use(plugins.paginate({
+      perPage: 10,
+      path: ":collection/page"
+    }))
+    .use(plugins.excerpts())
+    .use(plugins.markdown({
+      gfm: true,
+      tables: true,
+      smartLists: true,
+      smartypants: true,
+      highlight: function (code, lang, callback) {
+        return require("highlight.js").highlightAuto(code).value;
+      }
+    }))
+    .use(plugins.permalinks({
+      pattern: ":collection/:title",
+      relative: true
+    }))
+    .use(plugins.pagedata())
+    // use handlebars on content
+    .use(plugins.inPlace({
+      engine: "handlebars",
+      pattern: "**/*.{md,html}"
+    }))
+    // use handlebars templates
+    .use(plugins.layouts({
+      engine: "handlebars"
+    }))
+    .use(plugins.eyeglass({
+      outputStyle: config.isProduction ? "compressed" : "expanded",
+      root: rootDir,
+      buildDir: "${source}/assets/",
+      assetsHttpPrefix: "assets"
+    }))
+    .use(plugins.autoprefixer())
+    .use(plugins.htmlMinifier())
+    .use(plugins.uglify())
+    .use(plugins.serve())
+    .destination("./dist")
+    .build(function(err) {
+      if (err) { throw err; }
+    });
+}
+
+
---
+++ src/content/posts/tmp.md
@@ -0,0 +1,11 @@
+---
+title: This is an example
+date: 2015-07-17
+layout: default.hbt
+---
+
+{{page.title}}
+
+```scss
+$color: red;
+```

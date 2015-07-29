ef296068f3909fc960a903d5f869dcb465b53996
Eugene ONeill <oneill.eugene@gmail.com>
2015-07-28 17:59:58 -0700

    adding comments for metalsmith plugin ordering

--- metalsmith/index.js
+++ metalsmith/index.js
@@ -27,7 +27,9 @@ module.exports = function(rootDir) {
       },
       livereload: !!config.livereload
     }) || noop)
+    // this allows us to update handlebars whenever a partial or helper changes
     .use(plugins.configureHandlebars(Handlebars, config))
+    // filter out files we don't want to process
     .use(plugins.filter())
     .use(config.isProd && plugins.drafts() || noop)
     .use(plugins.collections({
@@ -59,6 +61,7 @@ module.exports = function(rootDir) {
         return require("highlight.js").highlightAuto(code).value;
       }
     }))
+    // gets the collection of tags (must happen AFTER #markdown)
     .use(plugins.tags({
       handle: "tags",
       //metadataKey: "alltags",
@@ -66,6 +69,7 @@ module.exports = function(rootDir) {
       // template to use for tag listing
       template: "topic.hbt"
     }))
+    // sets data to be used by #permalinks
     .use(plugins.permalinksInfo())
     .use(plugins.permalinks({
       pattern: ":collection/:permaname",
@@ -77,25 +81,37 @@ module.exports = function(rootDir) {
       engine: "handlebars",
       pattern: "**/*.{md,html}"
     }))
-    // extract excerpts right before we do the layouts
+    // the order here kinda matters
+    // extract the excerpts (this must happen AFTER #markdown and #inPlace)
     .use(plugins.excerpt())
+    // fix the collection page data which is now out-of-date (this must happen AFTER #collections, #excerpt, #permalinks, and #pagedata)
     .use(plugins.fixCollections())
+    // get topics from the collections (must happen AFTER #fixCollections)
     .use(plugins.collectionTopics())
+    // set information about tags (must happen AFTER #collectionTopics)
     .use(plugins.tagdata())
+    // set the sidebar information (must happen AFTER #collectionTopics and #tagdata)
     .use(plugins.aside(config))
+    // get a list of all the pages (should be the LAST thing we do before we render templates)
     .use(plugins.allpages())
+
     // use handlebars for layout
-    .use(plugins.templateToLayout())
+    .use(plugins.templateToLayout())  // must happen AFTER #tags, which uses the `template` key instead of `layout`
     .use(plugins.layouts({
       engine: "handlebars"
     }))
+
+    // this can happen whenever
     .use(plugins.eyeglass({
       outputStyle: config.isProd ? "compressed" : "expanded",
       root: rootDir,
       buildDir: "${source}/assets/",
       assetsHttpPrefix: "assets"
     }))
+    // use auto-prefixer (must happen AFTER #eyeglass)
     .use(plugins.autoprefixer())
+
+    // this is all post-process stuff, which should happen at the very end
     .use(config.isProd && plugins.htmlMinifier() || noop)
     .use(config.isProd && plugins.uglify() || noop)
     .use(config.isServer && plugins.serve() || noop)

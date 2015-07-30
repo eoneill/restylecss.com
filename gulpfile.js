cbb981250c9deeb60678a3e1a8f861bee72ca4ff
Eugene ONeill <oneill.eugene@gmail.com>
2015-07-30 16:41:11 -0700

    fixing sassdoc

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var gulp = require("gulp");
 
 require("./tasks/build")(gulp);
 require("./tasks/sassdoc")(gulp, ["build"]);
-require("./tasks/deploy")(gulp, ["build", "sassdoc"]);
+require("./tasks/deploy")(gulp, ["sassdoc"]);
 
 gulp.task("default", ["deploy"]);

086d8028845865ed066a1cc12d24ff0417c6716e
Eugene ONeill <oneill.eugene@gmail.com>
2015-08-01 18:44:25 -0700

    adding real support for multi-version docs

--- config.js
+++ config.js
@@ -72,6 +72,11 @@ var config = {
   isProd: false,
   today: Date.now(),
   dest: "./tmp/dist",
+  documentedVersions: [
+    "0.1.4",
+    "0.1.5",
+    "0.1.6"
+  ],
 
   environments: {
     production: {
--- tasks/sassdoc.js
+++ tasks/sassdoc.js
@@ -1,20 +1,54 @@
 "use strict";
 
 var sassdoc = require("sassdoc");
+var fs = require("fs-extra");
+var exec = require("child_process").execSync;
+var config = require("../config")();
 
-var restylePath = "./node_modules/eyeglass-restyle/";
-var waitDelay = 1000;
+var installPath = "./tmp/";
+var destPath = "./src/api/"
+var versions = config.documentedVersions;
+var stableVersion = config.stableVersion || versions[versions.length - 1];
+
+var gitRepo = "git@github.com:eoneill/eyeglass-restyle.git";
 
 module.exports = function(gulp, depends) {
   gulp.task("sassdoc", depends || [], function () {
-    var stream = sassdoc({
-      dest: "./src/api",
-      package: restylePath + "package.json"
+    var jobs = [];
+
+    function addJob(version, isStable) {
+      var path = installPath + "eyeglass-restyle@" + version + "/";
+
+      // if it already exists, don't download it again
+      if (!fs.existsSync(path)) {
+        console.log("downloading eyeglass-restyle@" + version + " before running SassDoc");
+        // otherwise, download it from git
+        exec("git clone --branch v" + version + " " + gitRepo + " " + path);
+      }
+
+      // create a new job for sassdoc
+      var job = sassdoc({
+        // only add a versioned directory if it's not flagged as stable
+        dest: destPath + (isStable ? "" : "v" + version),
+        package: path + "package.json"
+      });
+      gulp.src(path + "sass/**/*.s[ac]ss")
+        .pipe(job);
+
+      // push the job onto the array
+      jobs.push(job.promise);
+    }
+
+    // remove the /api dir
+    fs.removeSync(destPath);
+
+    // create a job for all
+    versions.map(function(version) {
+      addJob(version);
     });
 
-    gulp.src(restylePath + "sass/**/*.s[ac]ss")
-      .pipe(stream);
+    addJob(stableVersion, true);
 
-    return stream.promise;
+    return Promise.all(jobs);
   });
 };

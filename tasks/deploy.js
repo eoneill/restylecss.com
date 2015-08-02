0a0762e4d91f4280902eafcf89f1dfe8a8cc11b7
Eugene ONeill <oneill.eugene@gmail.com>
2015-08-01 23:56:19 -0700

    fix deploy for reals

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,18 +3,28 @@
 var ghPages = require("gulp-gh-pages");
 var merge = require("lodash.merge");
 var config = require("../config");
+var fs = require("fs-extra");
 
 module.exports = function(gulp, depends) {
   function createTask(name, options) {
+    options = merge({
+      cacheDir: "./tmp/.ghpages"
+    }, options);
+
     gulp.task(name, depends, function() {
+      if (options.clean && options.cacheDir) {
+        fs.removeSync(options.cacheDir);
+      }
+
       return gulp.src(config().dest + "/**/*")
-        .pipe(ghPages(merge({
-          cacheDir: "./tmp/.ghpages"
-        }, options)));
+        .pipe(ghPages(options));
     });
   }
 
-  createTask("deploy");
+  createTask("deploy", {
+    push: true,
+    clean: true
+  });
   createTask("deploy:dry", {
     push: false
   });
